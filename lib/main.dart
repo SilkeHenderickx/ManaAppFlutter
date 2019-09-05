419589bd640dddea75ac6f8eb137bf387ea5a3e5
Silke <silke.henderickx@gmail.com>
2019-09-06 00:09:38 +0200

    Home widget extends StatelessWidget => hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text(
-        'Mana App',
-        style: TextStyle(
-          fontFamily: 'PermanentMarker',
-          fontSize: 24,
-        ),
-      ),
-      centerTitle: true,
-      backgroundColor: Colors.teal[300],
-    ),
-    body: Center(
-      child: Text(
-        'Hello',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.grey[800],
-          fontFamily: 'PermanentMarker',
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('Click'),
-      backgroundColor: Colors.teal[300],
-    ),
-  ),
+  home: Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Mana App',
+          style: TextStyle(
+            fontFamily: 'PermanentMarker',
+            fontSize: 24,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.teal[300],
+      ),
+      body: Center(
+        child: Text(
+          'Hello',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.grey[800],
+            fontFamily: 'PermanentMarker',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('Click'),
+        backgroundColor: Colors.teal[300],
+      ),
+    );
+  }
+}
